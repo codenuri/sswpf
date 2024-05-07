2d6cfcfe05890a44ad5adf1b7a7020c86a28ecd5
ecourse <smkang@codenuri.co.kr>
2024-05-07 17:10:43 +0900

    Add files via upload

--- DAY1/LINQ2.cs
+++ DAY1/LINQ2.cs
@@ -10,8 +10,11 @@ class Program
         // LINQ : collection 을 바라보는 시각(view)를 제공하는것
 
         var view1 = arr.Where(n => n % 2 == 0);
+        var view2 = arr.Cast<double>(); // double 로 바꾸서 바라보는 View
 
-        foreach( var e in view1)
+        arr[0] = 6;
+
+        foreach ( var e in view1)
         {
             Console.WriteLine(e);
         }
