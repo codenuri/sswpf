100235e2fd0c93b1688bf525b2e89a632631b7af
ecourse <smkang@codenuri.co.kr>
2024-05-08 15:57:58 +0900

    Add files via upload

---
+++ DAY2/1_XAML_LOAD/Step2.cs
@@ -0,0 +1,45 @@
+﻿//Step1.cs - 어제 소스에서 skeleton 복사해 오세요
+// 없으신분은 github.com/codenuri/sswpf 참고
+
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+
+class MainWindow : Window
+{
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseLeftButtonDown(e);
+
+        Console.WriteLine("MouseDown");
+    }
+
+    public MainWindow()
+    {
+    }
+}
+
+class App : Application
+{
+    public App() { }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        Window win = null;
+
+        using (FileStream fs = new FileStream("../../../Step2.xaml", FileMode.Open))
+        {
+            win = (Window)XamlReader.Load(fs);
+        }
+
+        app.Run(win);
+    }
+}
