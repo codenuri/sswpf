c4268bfb127ba7eb9ec5fcd1f3b864d0500cf02d
ecourse <smkang@codenuri.co.kr>
2024-05-09 16:38:13 +0900

    Add files via upload

---
+++ DAY3/Step7_Bind2/Ex4_ItemSource4.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Step7_Bind2
+{
+
+    public partial class Ex4_ItemSource4 : Window
+    {
+        private ObservableCollection<People> col = new ObservableCollection<People>();
+
+        public Ex4_ItemSource4()
+        {
+            InitializeComponent();
+
+            col.Add(new People { Name = "name1", Address = "address1" });
+            col.Add(new People { Name = "name2", Address = "address2" });
+            col.Add(new People { Name = "name3", Address = "address3" });
+
+            datagrid.ItemsSource = col;
+        }
+    }
+}
