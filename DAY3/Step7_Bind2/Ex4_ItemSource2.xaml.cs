b602cb81af43ebc1a098f2fc787a274429b56ffa
ecourse <smkang@codenuri.co.kr>
2024-05-09 15:52:49 +0900

    Add files via upload

---
+++ DAY3/Step7_Bind2/Ex4_ItemSource2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Step7_Bind2
+{
+    public partial class Ex4_ItemSource2 : Window
+    {
+        public Ex4_ItemSource2()
+        {
+            InitializeComponent();
+
+            // Fonts.SystemFontFamilies : 시스템에 설치된 폰트 목록을 가진
+            //                            Collection 입니다.
+            listbox.ItemsSource = Fonts.SystemFontFamilies;
+        }
+    }
+}
